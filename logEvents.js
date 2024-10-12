460b4955d2f458d899f090ff1d959416ec6c1d57
Victor Mwadime <victormwadime@gmail.com>
2024-10-12 17:56:22 +0300

    Update logEvents.js

--- logEvents.js
+++ logEvents.js
@@ -10,7 +10,9 @@ const path = require("path");
 const logEvents = async (message) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
   const logItem = `${dateTime}\t${uuidv4()}\t${message}`;
+
   console.log(logItem);
+
   try {
     if (!fs.existsSync(path.join(__dirname, "logs"))) {
       await fsPromises.mkdir(path.join(__dirname, "logs"));

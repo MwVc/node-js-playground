d3df291e186617009e8d01eb912f282e5732d513
Victor Mwadime <victormwadime@gmail.com>
2024-10-17 20:28:11 +0300

    learn exporting arrays and objects

---
+++ module_two.js
@@ -0,0 +1,9 @@
+module.exports.items = ["item1", "item2"];
+const person = {
+  name: "mwadime",
+  email: "victormwadime@gmail.com",
+};
+
+module.exports.backendDeveloper = person;
+
+console.log(module);

7a38ea9c60554335326cca72e346d2d0b03d45de
Victor Mwadime <victormwadime@gmail.com>
2024-10-17 20:13:33 +0300

    Export function

--- module_one.js
+++ module_one.js
@@ -1,9 +1,9 @@
 // Modules
 // CommonJS, every file is a module (by default)
-const name = "John";
 
 const sayHi = function (name) {
   console.log(`Hello there ${name}`);
 };
 
-sayHi(name);
+module.exports = sayHi;
+console.log(module);

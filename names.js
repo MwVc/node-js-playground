4e9e1d8c8fd3b733a76c7cc422f7a30865be4b3b
Victor Mwadime <victormwadime@gmail.com>
2024-10-17 20:14:29 +0300

    create names module

---
+++ names.js
@@ -0,0 +1,10 @@
+// local
+const john = "John";
+
+// Sharing these with our rest of application
+const peter = "Peter";
+const mary = "Mary";
+
+module.exports = { peter, mary };
+
+console.log(module);

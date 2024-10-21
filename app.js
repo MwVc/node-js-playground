084e572ea9fa16075cc8f6874a497584ce4fc6d0
Victor Mwadime <victormwadime@gmail.com>
2024-10-21 11:36:46 +0300

    learn about path module

--- app.js
+++ app.js
@@ -28,4 +28,13 @@ const currentOS = {
 
 // PATH MODULE
 const path = require("path");
-console.log(path.join());
+// console.log(path.sep);
+
+const filePath = path.join("/content", "subfolder", "test.txt");
+// console.log(filePath);
+
+const base = path.basename(filePath);
+// console.log(base);
+
+const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
+console.log(absolute);

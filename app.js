257b5d2a55f456d01af111226a159bbe659e5581
Victor Mwadime <victormwadime@gmail.com>
2024-10-21 11:12:38 +0300

    info about the os module

--- app.js
+++ app.js
@@ -6,7 +6,26 @@
 // modules      - info about current module(file)
 // process      - info about enviroment where the program is being executed
 
-const names = require("./names");
-const sayHi = require("./module_one");
+// const names = require("./names");
+// const sayHi = require("./module_one");
+// const data = require("./module_two");
 
-sayHi(names.peter);
+// OS MODULE
+const os = require("os");
+
+// Info about the user
+const user = os.userInfo();
+
+// How long the system uptime in seconds
+const uptime = os.uptime();
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMemory: os.totalmem(),
+  freeMem: os.freemem(),
+};
+
+// PATH MODULE
+const path = require("path");
+console.log(path.join());

2efc1ce43d1a0efe955fadbc95997913a13da418
Victor Mwadime <victormwadime@gmail.com>
2024-10-21 12:13:26 +0300

    create module

---
+++ module_four.js
@@ -0,0 +1,40 @@
+// GLOBALS - NO WINDOW !!!
+
+// __dirname    - path to current directory
+// __filename   - file name
+// require      - function to use modules(CommonJS)
+// modules      - info about current module(file)
+// process      - info about enviroment where the program is being executed
+
+// const names = require("./names");
+// const sayHi = require("./module_one");
+// const data = require("./module_two");
+
+// OS MODULE
+const os = require("os");
+
+// Info about the user
+const user = os.userInfo();
+
+// How long the system uptime in seconds
+const uptime = os.uptime();
+
+const currentOS = {
+  name: os.type(),
+  release: os.release(),
+  totalMemory: os.totalmem(),
+  freeMem: os.freemem(),
+};
+
+// PATH MODULE
+const path = require("path");
+// console.log(path.sep);
+
+const filePath = path.join("/content", "subfolder", "test.txt");
+// console.log(filePath);
+
+const base = path.basename(filePath);
+// console.log(base);
+
+const absolute = path.resolve(__dirname, "content", "subfolder", "test.txt");
+console.log(absolute);

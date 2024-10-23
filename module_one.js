cbaf8499fe2b146fc69004ec5bacbd4cdccaba4d
Victor Mwadime <victormwadime@gmail.com>
2024-10-23 10:34:25 +0300

    delete all files and start afresh

--- module_one.js
+++
@@ -1,9 +0,0 @@
-// Modules
-// CommonJS, every file is a module (by default)
-
-const sayHi = function (name) {
-  console.log(`Hello there ${name}`);
-};
-
-module.exports = sayHi;
-console.log(module);
--- module_two.js
+++
@@ -1,9 +0,0 @@
-module.exports.items = ["item1", "item2"];
-const person = {
-  name: "mwadime",
-  email: "victormwadime@gmail.com",
-};
-
-module.exports.backendDeveloper = person;
-
-console.log(module);

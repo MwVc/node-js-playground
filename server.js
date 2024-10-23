0ad669a4345791753c7922958f6b5914b8220fe1
Victor Mwadime <victormwadime@gmail.com>
2024-10-23 13:07:09 +0300

    create server routing, check for GET and POST reuest

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 import http from "http";
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req, res) => {
-  //   res.setHeader("Content-type", "text/html");
-  //   res.statusCode = 404;
-
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("<h1>Basic information site</h1>");
+  try {
+    //  Check if GET request
+    if (req.method === "GET") {
+      if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>This is my personal homepage</h1>");
+      } else if (req.url === "/about") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("<h1>This is my personal homepage</h1>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end(`<h1>Page Not Found!!</h1>
+          <a href="/">Go Back Home</a>
+          `);
+      }
+    } else {
+      throw new Error("Method not allowed");
+    }
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("Server Error");
+  }
 });
 
 server.listen(PORT, () => {
